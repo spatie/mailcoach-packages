d67eb49b062b5179cc42770f72603f8ef782c48a
freek <freek@spatie.be>
2022-01-19 16:18:34 +0100

    wip

--- packages/laravel-mailcoach-mailgun-feedback/src/StoreTransportMessageId.php
+++ packages/laravel-mailcoach-mailgun-feedback/src/StoreTransportMessageId.php
@@ -19,7 +19,7 @@ class StoreTransportMessageId
         /** @var \Spatie\Mailcoach\Domain\Shared\Models\Send $send */
         $send = $event->data['send'];
 
-        $transportMessageId = $event->message->getHeaders()->get('X-Mailgun-Message-ID')->getFieldBody();
+        $transportMessageId = $event->message->getHeaders()->get('X-Mailgun-Message-ID')->getBodyAsString();
 
         $transportMessageId = ltrim($transportMessageId, '<');
         $transportMessageId = rtrim($transportMessageId, '>');
