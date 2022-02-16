83c51f0ef76a88b5db16dac9968159a92f288a94
Rias <hey@rias.be>
2022-02-16 09:20:41 +0100

    Fix tests

--- packages/laravel-mailcoach-mailgun-feedback/tests/StoreTransportMessageIdTest.php
+++ packages/laravel-mailcoach-mailgun-feedback/tests/StoreTransportMessageIdTest.php
@@ -3,9 +3,14 @@
 namespace Spatie\MailcoachMailgunFeedback\Tests;
 
 use Illuminate\Mail\Events\MessageSent;
+use Illuminate\Mail\SentMessage;
+use Illuminate\Support\Env;
 use Spatie\Mailcoach\Domain\Shared\Models\Send;
+use Symfony\Component\Mailer\Envelope;
+use Symfony\Component\Mime\Address;
 use Symfony\Component\Mime\Email;
 use Symfony\Component\Mime\Part\TextPart;
+use Symfony\Component\Mime\RawMessage;
 
 class StoreTransportMessageIdTest extends TestCase
 {
@@ -15,6 +20,19 @@ class StoreTransportMessageIdTest extends TestCase
         $pendingSend = Send::factory()->create();
         $message = (new Email())->setBody(new TextPart('body'));
         $message->getHeaders()->addTextHeader('X-Mailgun-Message-ID', '1234');
+        $message->sender('john@doe.com');
+        $message->to('john@doe.com');
+        $message->text('body');
+
+        $symfonySentMessage = new \Symfony\Component\Mailer\SentMessage(
+            message: $message,
+            envelope: new Envelope(
+                sender: new Address('john@doe.com'),
+                recipients: [new Address('john@doe.com')]
+            )
+        );
+
+        $message = new SentMessage($symfonySentMessage);
 
         event(new MessageSent($message, [
             'send' => $pendingSend,
