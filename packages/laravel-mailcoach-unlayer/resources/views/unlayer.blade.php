bd8e870cd94d3ff735d019ce61896938a6bcc462
Rias <hey@rias.be>
2022-05-16 14:01:40 +0200

    Unlayer editor to Alpine & Livewire

--- packages/laravel-mailcoach-unlayer/resources/views/unlayer.blade.php
+++ packages/laravel-mailcoach-unlayer/resources/views/unlayer.blade.php
@@ -1,131 +1,100 @@
-@push('endHead')
-    <script id="unlayer" src="https://editor.unlayer.com/embed.js" defer></script>
-@endpush
+<div class="min-h-screen">
+    <script>
+        function loadTemplate() {
+            document.getElementById('unlayer_template_error').classList.add('hidden');
+            const slug = document.getElementById('unlayer_template').value;
 
-<script>
-    window.unlayerInitialized = false;
-
-    document.getElementById('unlayer').addEventListener('load', initUnlayer);
-
-    document.addEventListener('turbo:before-visit', confirmBeforeLeaveAndDestroyUnlayer);
-    document.addEventListener("turbo:load", initUnlayer);
-    window.addEventListener('beforeunload', confirmBeforeLeaveAndDestroyUnlayer);
-
-    function loadTemplate() {
-        document.getElementById('unlayer_template_error').classList.add('hidden');
-        const slug = document.getElementById('unlayer_template').value;
-
-        fetch('https://api.graphql.unlayer.com/graphql', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                query: `
-                    query StockTemplateLoad($slug: String!) {
-                      StockTemplate(slug: $slug) {
-                        StockTemplatePages {
-                          design
-                        }
-                      }
-                    }
-                  `,
-                variables: {
-                    slug: slug,
-                },
-            }),
-        })
-            .then((res) => res.json())
-            .then((result) => {
-                if (! result.data.StockTemplate) {
-                    document.getElementById('unlayer_template_error').innerHTML = '{{ __('mailcoach - Template not found.') }}';
-                    document.getElementById('unlayer_template_error').classList.remove('hidden');
-                    return;
-                }
-
-                unlayer.loadDesign(result.data.StockTemplate.StockTemplatePages[0].design);
-                document.querySelector('[data-modal="load-unlayer-template"]').dispatchEvent(new Event('dismiss'));
-            });
-    }
-
-    function initUnlayer() {
-        document.getElementById('load-template').addEventListener('click', loadTemplate);
-
-        if (window.unlayerInitialized || unlayer.init === undefined) {
-            return;
-        }
-
-        unlayer.init(@json($options));
-
-        window.unlayerInitialized = true;
-
-        unlayer.loadDesign({!! $structuredHtml !!});
-
-        unlayer.registerCallback('image', (file, done) => {
-            let data = new FormData();
-            data.append('file', file.attachments[0]);
-
-            fetch('{{ route('mailcoach-unlayer.upload') }}', {
+            fetch('https://api.graphql.unlayer.com/graphql', {
                 method: 'POST',
                 headers: {
-                    'Accept': 'application/json',
-                    'X-CSRF-TOKEN': '{{ csrf_token() }}',
+                    'Content-Type': 'application/json',
                 },
-                body: data
-            }).then(response => {
-                // Make sure the response was valid
-                if (response.status >= 200 && response.status < 300) {
-                    return response.json()
-                }
+                body: JSON.stringify({
+                    query: `
+                        query StockTemplateLoad($slug: String!) {
+                          StockTemplate(slug: $slug) {
+                            StockTemplatePages {
+                              design
+                            }
+                          }
+                        }
+                      `,
+                    variables: {
+                        slug: slug,
+                    },
+                }),
+            })
+                .then((res) => res.json())
+                .then((result) => {
+                    if (! result.data.StockTemplate) {
+                        document.getElementById('unlayer_template_error').innerHTML = '{{ __('mailcoach - Template not found.') }}';
+                        document.getElementById('unlayer_template_error').classList.remove('hidden');
+                        return;
+                    }
 
-                let error = new Error(response.statusText);
-                error.response = response;
-                throw error
-            }).then(data => done({ progress: 100, url: data.url }))
-        });
-
-        const mergeTags = {};
-        @foreach ($replacers as $replacerName => $replacerDescription)
-            mergeTags["{{ $replacerName }}"] = {
-                name: "{{ $replacerName }}",
-                value: "::{{ $replacerName }}::"
-            };
-        @endforeach
-
-        unlayer.setMergeTags(mergeTags);
-
-        document.getElementById('save').addEventListener('click', event => {
-            event.preventDefault();
-
-            unlayer.exportHtml(function(data) {
-                document.getElementById('html').value = data.html;
-                document.getElementById('structured_html').value = JSON.stringify(data.design);
-                document.getElementById('html').dataset.dirty = "";
-                document.querySelector('main form').submit();
-            });
-        });
-
-        unlayer.addEventListener('design:updated', function(data) {
-            document.getElementById('html').dataset.dirty = "dirty";
-        });
-    }
-
-    function confirmBeforeLeaveAndDestroyUnlayer(event) {
-        if (document.getElementById('html').dataset.dirty === "dirty" && ! confirm('Are you sure you want to leave this page? Any unsaved changes will be lost.')) {
-            event.preventDefault();
-            return;
+                    unlayer.loadDesign(result.data.StockTemplate.StockTemplatePages[0].design);
+                    Alpine.store('modals').close('load-unlayer-template');
+                });
         }
 
-        window.unlayerInitialized = false;
+        window.init = function() {
+            document.getElementById('load-template').addEventListener('click', loadTemplate);
 
-        document.removeEventListener('turbo:before-visit', confirmBeforeLeaveAndDestroyUnlayer);
-        document.removeEventListener("turbo:load", initUnlayer);
-        window.removeEventListener('beforeunload', confirmBeforeLeaveAndDestroyUnlayer);
-        document.getElementById('load-template').removeEventListener('click', loadTemplate);
-    }
+            unlayer.init(@json($options));
 
-</script>
-<div class="h-full">
+            unlayer.loadDesign(JSON.parse(JSON.stringify(this.json)));
+
+            unlayer.registerCallback('image', (file, done) => {
+                let data = new FormData();
+                data.append('file', file.attachments[0]);
+
+                fetch('{{ action(\Spatie\Mailcoach\Http\Api\Controllers\UploadsController::class) }}', {
+                    method: 'POST',
+                    headers: {
+                        'Accept': 'application/json',
+                        'X-CSRF-TOKEN': '{{ csrf_token() }}',
+                    },
+                    body: data
+                })
+                .then(response => {
+                    // Make sure the response was valid
+                    if (response.status >= 200 && response.status < 300) {
+                        return response.json()
+                    }
+
+                    let error = new Error(response.statusText);
+                    error.response = response;
+                    throw error
+                }).then(data => done({ progress: 100, url: data.file.url }))
+            });
+
+            const mergeTags = {};
+            @foreach ($replacers as $replacerName => $replacerDescription)
+                mergeTags["{{ $replacerName }}"] = {
+                    name: "{{ $replacerName }}",
+                    value: "::{{ $replacerName }}::"
+                };
+            @endforeach
+
+            unlayer.setMergeTags(mergeTags);
+
+            const component = this;
+            unlayer.addEventListener('design:updated', () => {
+                unlayer.exportHtml(function(data) {
+                    component.html = data.html;
+                    component.json = data.design;
+                });
+            });
+
+            unlayer.addEventListener('design:loaded', function(data) {
+                unlayer.exportHtml(function(data) {
+                    component.html = data.html;
+                    component.json = data.design;
+                });
+            });
+        }
+    </script>
+
     <div class="form-row max-w-full h-full">
         <label class="label" for="html">{{ __('Body') }}</label>
         @isset($errors)
@@ -133,20 +102,29 @@
                 <p class="form-error" role="alert">{{ $message }}</p>
             @enderror
         @endisset
-        <div class="overflow-hidden -mx-10 h-full">
-            <div id="editor" class="h-full -ml-2 pr-3 py-1" style="min-height: 75vh"></div>
+        <div wire:ignore x-data="{
+            html: @entangle('templateFieldValues.html'),
+            json: @entangle('templateFieldValues.json'),
+            init: init,
+        }" class="overflow-hidden -mx-10 h-full">
+            <div id="editor" class="h-full -ml-2 pr-3 py-1" style="min-height: 75vh; height: 75vh"></div>
         </div>
-        <input type="hidden" name="html" id="html" value="{{ $html }}">
-        <input type="hidden" name="structured_html" id="structured_html" value="{{ json_encode($structuredHtml) }}">
     </div>
-</div>
 
-<div class="form-buttons">
-    <x-mailcoach::button id="save" :label="__('Save content')"/>
-    @if ($showTestButton)
-        <x-mailcoach::button-secondary data-modal-trigger="send-test" :label="__('Send Test')"/>
-    @endif
-    <x-mailcoach::button-secondary data-modal-trigger="load-unlayer-template" :label="__('mailcoach - Load Unlayer template')"/>
+    <x-mailcoach::campaign-replacer-help-texts/>
+
+    <div class="form-buttons">
+        <x-mailcoach::button wire:click="save" :label="__('mailcoach - Save content')"/>
+
+        <x-mailcoach::button x-on:click.prevent="$wire.save() && $store.modals.open('send-test')" class="ml-2" :label="__('mailcoach - Save and send test')"/>
+        <x-mailcoach::modal name="send-test">
+            <livewire:mailcoach::send-test :model="$sendable" />
+        </x-mailcoach::modal>
+
+        <x-mailcoach::button-secondary x-on:click.prevent="$store.modals.open('preview')" :label="__('mailcoach - Preview')"/>
+        <x-mailcoach::preview-modal name="preview" :html="$fullHtml" :title="__('mailcoach - Preview') . ' - ' . $sendable->subject" />
+        <x-mailcoach::button-secondary x-on:click.prevent="$store.modals.open('load-unlayer-template')" :label="__('mailcoach - Load Unlayer template')"/>
+    </div>
 </div>
 
 @push('modals')
