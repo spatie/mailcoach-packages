7ddcdc5439a31df39a9d2bf08c5a868a6cf12d02
Rias <hey@rias.be>
2022-08-22 11:08:56 +0200

    Register sendgrid mailer

--- packages/laravel-mailcoach-sendgrid-feedback/src/MailcoachSendgridFeedbackServiceProvider.php
+++ packages/laravel-mailcoach-sendgrid-feedback/src/MailcoachSendgridFeedbackServiceProvider.php
@@ -4,8 +4,11 @@ namespace Spatie\MailcoachSendgridFeedback;
 
 use Illuminate\Mail\Events\MessageSending;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
+use Symfony\Component\Mailer\Bridge\Sendgrid\Transport\SendgridTransportFactory;
+use Symfony\Component\Mailer\Transport\Dsn;
 
 class MailcoachSendgridFeedbackServiceProvider extends ServiceProvider
 {
@@ -14,5 +17,13 @@ class MailcoachSendgridFeedbackServiceProvider extends ServiceProvider
         Route::macro('sendgridFeedback', fn (string $url) => Route::post("{$url}/{mailer?}", '\\' . SendgridWebhookController::class));
 
         Event::listen(MessageSending::class, AddUniqueArgumentsMailHeader::class);
+
+        Mail::extend('sendgrid', function (array $config) {
+            $key = $config['key'] ?? config('services.sendgrid.key');
+
+            return (new SendgridTransportFactory())->create(
+                Dsn::fromString("sendgrid+api://{$key}@default")
+            );
+        });
     }
 }
