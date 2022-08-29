756e5376c6349368c8c8f6d6dc5f94621e8e9d1b
Rias <hey@rias.be>
2022-08-29 10:01:10 +0200

    Use package action to render markdown

--- packages/laravel-mailcoach-markdown-editor/src/RenderMarkdownController.php
+++ packages/laravel-mailcoach-markdown-editor/src/RenderMarkdownController.php
@@ -3,28 +3,17 @@
 namespace Spatie\MailcoachMarkdownEditor;
 
 use Illuminate\Http\Request;
-use League\CommonMark\Extension\Table\TableExtension;
-use Spatie\LaravelMarkdown\MarkdownRenderer;
+use Spatie\Mailcoach\Domain\Shared\Actions\RenderMarkdownToHtmlAction;
 
 class RenderMarkdownController
 {
-    public function __invoke(Request $request, MarkdownRenderer $renderer): string
+    public function __invoke(Request $request, RenderMarkdownToHtmlAction $action): string
     {
         $data = $request->validate([
             'markdown' => ['required'],
             'theme' => ['nullable'],
         ]);
 
-        if (in_array(\Spatie\SidecarShiki\Functions\HighlightFunction::class, config('sidecar.functions', []))) {
-            $renderer
-                ->highlightCode(false)
-                ->addExtension(new \Spatie\SidecarShiki\Commonmark\HighlightCodeExtension($data['theme']));
-        }
-
-        return $renderer
-            ->disableAnchors()
-            ->addExtension(new TableExtension())
-            ->highlightTheme($data['theme'] ?? 'nord')
-            ->toHtml($data['markdown']);
+        return $action->execute($data['markdown'], $data['theme'])->toHtml();
     }
 }
