37402d83629569be14c9fc00be6fa817113022fe
Rias <hey@rias.be>
2022-10-25 10:08:23 +0200

    Better Unlayer styling

--- packages/laravel-mailcoach-unlayer/resources/views/unlayer.blade.php
+++ packages/laravel-mailcoach-unlayer/resources/views/unlayer.blade.php
@@ -13,7 +13,7 @@
         }
     </style>
 @endpush
-<div id="unlayer-wrapper" class="min-h-screen">
+<div id="unlayer-wrapper">
     <script>
         function loadTemplate() {
             document.getElementById('unlayer_template_error').classList.add('hidden');
@@ -124,13 +124,13 @@
         }
     </script>
 
-    <div class="max-w-full min-h-screen flex flex-col">
+    <div class="max-w-full flex flex-col">
         <div wire:ignore x-data="{
             html: @entangle('templateFieldValues.html'),
             json: @entangle('templateFieldValues.json'),
             init: init,
-        }" class="overflow-hidden -mx-10 flex-1 h-full">
-            <div id="editor" class="h-full -ml-2 pr-3 py-1" style="min-height: 90vh; height: 90vh" data-dirty-check></div>
+        }" class="overflow-hidden -mx-10 flex-1 h-full mb-6">
+            <div id="editor" class="h-full -ml-2 pr-3 py-1" style="min-height: 75vh; height: 75vh" data-dirty-check></div>
         </div>
 
         <x-mailcoach::replacer-help-texts :model="$model" />
